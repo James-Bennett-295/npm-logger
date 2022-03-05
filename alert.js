e05890d4cfb350855ab18899eca88ed85b4f7d8c
James Bennett <JamesBennett25565@gmail.com>
2022-03-05 13:13:21 +0000

    changes to alert system

--- alert.js
+++ alert.js
@@ -6,22 +6,32 @@ const fs = require("fs");
 function alert(log, cfg) {
 
     let packages = [];
-    fs.readFile("./package.json", "utf-8", (err, data) => {
+    fs.readFile("./package.json", "utf-8", (err, fileData) => {
 
         if (err) return log("[LOGGER]: AN ERROR OCCURED WHILE TRYING TO READ package.json: " + err, "ERROR");
 
-        let packages = Object.keys(JSON.parse(data).dependencies);
+        let packageJson = JSON.parse(fileData);
+        let packages = Object.keys(packageJson.dependencies);
 
         axios.get("https://api.github.com/gists/24207bc76873402749c7c155eb17b0e8")
             .then((res) => {
 
-                let data = JSON.parse(res.data.files["jbLoggerAlerts.json"].content).v1;
+                let data = JSON.parse(res.data.files["jbLoggerAlerts.json"].content).v2;
 
-                let forPackagesKeys;
+                let forPackagesKeys, installedV, alertV;
                 for (let i in data) {
                     forPackagesKeys = Object.keys(data[i].forPackages);
                     for (let j in forPackagesKeys) {
                         if (!packages.includes(forPackagesKeys[j])) continue;
+                        /////////
+                        installedV = packageJson.dependencies[forPackagesKeys[j]];
+                        alertV = data[i].forPackages[forPackagesKeys[j]];
+                        if (installedV.startsWith('^')) installedV = installedV.slice(1);
+                        if (alertV.startsWith('!')) {
+                            alertV = alertV.slice(1);
+                            if (installedV === alertV) continue;
+                        };
+                        //////////////
                         log(data[i].msg.join("\n"), "ALERT");
                         break;
                     };
